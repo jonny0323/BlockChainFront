255cbe51928308c67ca296c282757b0edc788a5a
TaeJoon Lee <taejoonlee@TaeJoonui-noteubug.local>
2025-11-24 13:18:59 +0900

    resolve

--- src/services/api.js
+++ src/services/api.js
@@ -56,7 +56,7 @@ export const isAdmin = () => {
         const base64Url = token.split('.')[1];
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
         const payload = JSON.parse(window.atob(base64));
-        return payload.isAdmin || false;
+        return payload.admin || false;
     } catch (error) {
         return false;
     }
